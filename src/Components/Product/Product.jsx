c63c0eef429c96dafb39511967cd273658f3a62e
unknown <rockydasgfx@gmail.com>
2022-06-03 01:39:23 +0600

    send data on product compo

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Product = () => {
+const Product = (props) => {
+  console.log(props.product);
   return (
     <div className="product">
       <h2>all product</h2>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import Selected from "../Selected/Selected";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div className="shop">
-      <h1>form Shop</h1>
-    </div>
+    <main>
+      <section className="shop">
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </section>
+      <section className="selected">
+        <Selected />
+      </section>
+    </main>
   );
 };
 
