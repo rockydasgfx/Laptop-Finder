509166a348a5743dfe488de28e6e7db1c1d60799
unknown <rockydasgfx@gmail.com>
2022-06-03 01:19:24 +0600

    lode data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import Header from "./Components/Header/Header";
 import Selected from "./Components/Selected/Selected";
 import Shop from "./Components/Shop/Shop";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  console.log(products);
   return (
     <div className="app">
       <Header />
       <main>
-        <section>
+        <section className="shop">
           <Shop />
         </section>
-        <section>
+        <section className="selected">
           <Selected />
         </section>
       </main>
---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <h2>all product</h2>
+    </div>
+  );
+};
+
+export default Product;
